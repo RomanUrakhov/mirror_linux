cbbf1f881331053a13a685c2cbd1b6f4298233d1
RomanUrakhov <urahovroman@gmail.com>
2020-04-25 15:33:24 +0500

    Added additional checks to the write_task_status

--- queries/task.py
+++ queries/task.py
@@ -1,4 +1,4 @@
-from models.models import User, Repository, Task
+from models.models import Repository, Task
 
 
 def get_task_query(id=1):
@@ -17,22 +17,28 @@ def get_task_count_query():
 
 
 def get_task_list_query(offset=0, limit=15):
-    taskList = []
+    task_list = []
     for task in Task.select().offset(offset).limit(limit):
-        taskList.append({
+        task_list.append({
             "id": int(task.__str__()),
             "mirror": task.repository,
             "message": task.message[:50],
             "user": task.user,
             "date": task.date,
         })
-    return taskList
+    return task_list
 
 
 def write_task_status(repo: Repository, msg):
     if msg:
-        if type(msg) in (bytes, bytearray):
-            msg = msg.decode("uft-8")
-        else:
-            msg = "Unreadable characters"
+        if type(msg) is not str:
+            if type(msg) is tuple:
+                if type(msg[1]) in (bytes, bytearray):
+                    msg = f"code: {msg[0]}, message: {msg[1].decode('utf-8')}"
+                elif type(msg[1]) is str:
+                    msg = f"code: {msg[0]}, message: {msg[1]}"
+                else:
+                    msg = "Unreadable characters"
+            else:
+                msg = "Unreadable characters"
         Task(repository=repo.name, message=msg, user=repo.user.username).save()
