080c8652681fa11f194a83d53de9550a901e4241
RomanUrakhov <urahovroman@gmail.com>
2020-06-05 14:34:04 +0500

    Replacements in methods

--- mirror/repository.py
+++ mirror/repository.py
@@ -1,10 +1,10 @@
-from models.models import Repository
-from mirror.zfs import Zfs
-from mirror.networking import Yum, Rsync
-from queries import task, repository
-from utils import loger
-import config
 import subprocess
+
+import config
+from mirror.networking import Yum, Rsync
+from mirror.zfs import Zfs
+from models.models import Repository
+from utils import loger
 
 
 class LinuxRepoManager:
@@ -16,97 +16,60 @@ class LinuxRepoManager:
     def __repr__(self):
         return f"Manager of {self.repo.name} repository"
 
-    def __user_script_update(self):
+    def __user_script_update(self, snap):
         if not config.on_update or config.on_update == "":
-            return 0, "ok"
+            return ""
         location = f"/{self.repo.mirror_zpool}/{self.repo.mirror_location}"
-        try:
-            return 0, subprocess.check_output([config.on_update, str(self.repo.get_id()), location])
-        except subprocess.CalledProcessError as e:
-            return 1, e.output
+        return subprocess.run(
+            [config.on_update, self.repo.mirror_zpool, self.repo.mirror_location, location, snap],
+            check=True,
+            stdout=subprocess.PIPE,
+            stderr=subprocess.STDOUT
+        ).stdout
 
     def __user_script_truncate(self):
         if not config.on_truncate or config.on_truncate == "":
-            return 0, "ok"
-        try:
-            return 0, subprocess.check_output([config.on_truncate, str(self.repo.get_id())])
-        except subprocess.CalledProcessError as e:
-            return 1, e.output
+            return ""
+        return subprocess.run(
+            [config.on_truncate, self.repo.mirror_zpool, self.repo.mirror_location],
+            check=True,
+            stdout=subprocess.PIPE,
+            stderr=subprocess.STDOUT
+        ).stdout
+
+    def filesystem_exist(self):
+        return self.zfs.filesystem_exist()
 
     @loger.state(start_msg="Create task started", end_msg="Create task finished")
     def create(self):
-        out = self.zfs.create()
-        task.write_task_status(self.repo, out)
-        if out[0]:
-            return 1
-        return 0
+        print("Zfs create")
+        print(self.zfs.create())
 
     @loger.state(start_msg="Update task started", end_msg="Update task finished")
     def update(self):
-        repository.set_next_update_date(repo=self.repo)
-        out = tuple()
         if self.repo.mirror_type == 1:
-            try:
-                yum = Yum(self.dir_path, self.repo.mirror_url)
-                out = yum.update()
-            except Exception as e:
-                out = 1, str(e)
+            print("Yum update")
+            yum = Yum(self.dir_path, self.repo.mirror_url)
+            print(yum.update())
         else:
+            print("Rsync update")
             rsync = Rsync(self.dir_path, self.repo.mirror_url)
-            out = rsync.update(self.repo.mirror_args)
-        task.write_task_status(self.repo, out)
-        if out[0]:
-            return 1
+            print(rsync.update(self.repo.mirror_args))
 
-        out = self.zfs.snapshot()
-        task.write_task_status(self.repo, out)
-        if out[0]:
-            return 2
+        print("Create snapshot")
+        print(self.zfs.snapshot())
 
-        out = self.zfs.snapshot_list()
-        task.write_task_status(self.repo, out)
-        if out[0]:
-            return 3
-        snap_list = out[1]
-
+        snap_list = self.zfs.snapshot_list()
         if len(snap_list) > self.repo.schedule_number:
-            out = self.zfs.destroy_snap(snap_list[-1])
-            task.write_task_status(self.repo, out)
-            if out[0]:
-                return 4
+            print(f"Delete snapshot {snap_list[-1]}")
+            print(self.zfs.destroy_snap(snap_list[-1]))
 
-        out = self.__user_script_update()
-        task.write_task_status(self.repo, out)
-        if out[0]:
-            return 5
-        return 0
+        print("User script on delete")
+        print(self.__user_script_update(snap_list[0]))
 
     @loger.state(start_msg="Delete task started", end_msg="Delete task finished")
     def delete(self):
-        out = self.zfs.delete()
-        task.write_task_status(self.repo, out)
-        if out[0]:
-            return 2
-        return 0
-
-    @loger.state(start_msg="Full Create task started", end_msg="Full Create task finished")
-    def full_create(self):
-        if self.create():
-            return 1
-        if self.update():
-            return 2
-        repository.init_mirror(self.repo)
-        return 0
-
-    @loger.state(start_msg="Reset task started", end_msg="Reset task finished")
-    def reset(self):
-        out = self.__user_script_truncate()
-        task.write_task_status(self.repo, out)
-        if out[0]:
-            return 1
-        if self.delete():
-            return 2
-        repository.deinit_mirror(self.repo)
-        if self.full_create():
-            return 3
-        return 0
+        print("User script on truncate")
+        print(self.__user_script_truncate())
+        print("Zfs delete")
+        print(self.zfs.delete())
